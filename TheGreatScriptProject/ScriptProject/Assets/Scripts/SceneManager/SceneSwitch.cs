568b37230db1c2694f2aa41e4bdba7bea4b37219
Yannick <ybroeks11@gmail.com>
2021-07-25 21:42:30 +0200

    SceneManager_FreeMoveCamera
    Added 2 scripts

---
+++ TheGreatScriptProject/ScriptProject/Assets/Scripts/SceneManager/SceneSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [Header("Scene number:")]
+    [SerializeField] int scene1;
+    [SerializeField] int scene2;
+
+    //SceneSwitch mogelijkheid voor op een knop
+    private void SceneSwitch1()
+    {
+        SceneManager.LoadScene(scene1);
+    }
+
+    private void SceneSwitch2()
+    {
+        SceneManager.LoadScene(scene2);
+    }
+
+}
